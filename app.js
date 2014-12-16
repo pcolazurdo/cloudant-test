90a15b0b69f1ff9edae7eca20a008a3e774d096c
@pcolazurdo <pcolazurdo@yahoo.com>
2014-12-16 17:56:31 -0300

    Updated twitter package to v.1.0.1

--- app.js
+++ app.js
@@ -198,7 +198,7 @@ twit.stream('user', {track:'pcolazurdo'}, function(stream) {
 				//console.log(typeof data.id_str);
 				console.log(util.inspect(data));
 		});
-    //stream.on('favorite', function(data) {
+    stream.on('favorite', function(data) {
     //    console.log(data.target_object.text);
     //    insert_doc(data, data.target_object.id_str, function (err, response) {
     //        console.log(err || response);
